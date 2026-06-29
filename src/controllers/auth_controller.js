77f93aed99ac5d806e08732c081986c3387dd6a8
Aditya <aadityash8804@gmail.com>
2026-06-30 01:31:45 +0530

    Fix authentication cookie for production

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -4,10 +4,10 @@ const User = require('../models/user')
 const validator = require('validator')
 
 // signup - API
-async function userSignUp(req,res){
-    try{
+async function userSignUp(req, res) {
+    try {
         validateSignUpData(req);
-        const {firstName,
+        const { firstName,
             lastName,
             email,
             password,
@@ -17,12 +17,12 @@ async function userSignUp(req,res){
             about,
             skills
         } = req.body;
-        const hashPassword = await bcrypt.hash(password,10);
+        const hashPassword = await bcrypt.hash(password, 10);
         const user = new User({
             firstName,
             lastName,
             email,
-            password : hashPassword,
+            password: hashPassword,
             age,
             gender,
             photoURL,
@@ -30,29 +30,34 @@ async function userSignUp(req,res){
             skills
         })
         await user.save();
-        
+
         res.status(201).send("User created Successfully!");
-    }catch(err){
+    } catch (err) {
         res.status(400).send("ERROR: " + err.message);
     }
 }
 
 // login - API
-async function userLogIn(req,res){
+async function userLogIn(req, res) {
     try {
-        const {email,password} = req.body
-        if(!validator.isEmail(email)) throw new Error("Invalid E-mail Address!")
-        
-        const user = await User.findOne({email: email});
-        if(!user) throw new Error("Invalid Credentials!")
-        
+        const { email, password } = req.body
+        if (!validator.isEmail(email)) throw new Error("Invalid E-mail Address!")
+
+        const user = await User.findOne({ email: email });
+        if (!user) throw new Error("Invalid Credentials!")
+
         const isPasswordValid = await user.validatePassword(password);
-        if(!isPasswordValid){
+        if (!isPasswordValid) {
             throw new Error("Invalid Credentials!");
-        }else{
+        } else {
             const token = await user.getJWT();
-            const expiryDate = new Date(Date.now() + 1000*60*60*7*24); //7 days
-            res.cookie("token",token,{expires: expiryDate})
+            const expiryDate = new Date(Date.now() + 1000 * 60 * 60 * 7 * 24); //7 days
+            res.cookie("token", token, {
+                expires: expiryDate,
+                httpOnly: true,
+                secure: true,
+                sameSite: "None"
+            });
 
             res.status(200).send(user);
         }
@@ -62,13 +67,13 @@ async function userLogIn(req,res){
 }
 
 // logout - API
-async function userLogOut(req,res){
+async function userLogOut(req, res) {
     try {
-        res.cookie("token",null,{expires: new Date(Date.now())})
+        res.cookie("token", null, { expires: new Date(Date.now()) })
         res.status(200).send("LoggedOut Successfully!");
     } catch (err) {
         res.status(400).send("ERROR: " + err.message)
     }
 }
 
-module.exports = {userSignUp, userLogIn, userLogOut}
+module.exports = { userSignUp, userLogIn, userLogOut }
